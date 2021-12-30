1764b528efb42d7b5602e741dd8c13a9d981ac0b
tsankaris <d.zangari@campus.unimib.it>
2021-12-30 17:18:43 +0100

    bella

---
+++ app/src/main/java/it/sal/disco/unimib/trovacampi/Calcio_activity.java
@@ -0,0 +1,14 @@
+package it.sal.disco.unimib.trovacampi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calcio_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calcio);
+    }
+}
