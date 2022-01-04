b5cc66e1007fa29837667490e5726a844100a7be
tsankaris <d.zangari@campus.unimib.it>
2022-01-04 17:56:14 +0100

    bella

--- app/src/main/java/it/sal/disco/unimib/trovacampi/Calcio_activity.java
+++ app/src/main/java/it/sal/disco/unimib/trovacampi/Calcio_activity.java
@@ -2,13 +2,29 @@ package it.sal.disco.unimib.trovacampi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Calcio_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calcio);
+        setContentView(R.layout.activity_calcio); };
+
+
+
+    public void goToSo (View view) {
+        goToUrl ( "http://stackoverflow.com/");
+    }
+
+    private void goToUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
