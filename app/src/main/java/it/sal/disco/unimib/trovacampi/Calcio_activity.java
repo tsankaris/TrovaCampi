45a67a8b43d810a0cbf454841f27bfc7b7566d68
lucagal284 <l.galliani@campus.unimib.it>
2022-01-05 16:24:23 +0100

    bottone funzionante

--- app/src/main/java/it/sal/disco/unimib/trovacampi/Calcio_activity.java
+++ app/src/main/java/it/sal/disco/unimib/trovacampi/Calcio_activity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+/*import android.widget.Button;*/
 
 
 public class Calcio_activity extends AppCompatActivity {
@@ -14,12 +14,12 @@ public class Calcio_activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calcio); };
+        setContentView(R.layout.activity_calcio); }
 
 
 
     public void goToSo (View view) {
-        goToUrl ( "http://stackoverflow.com/");
+        goToUrl ( "https://docs.google.com/forms/d/e/1FAIpQLScrtyp4cQ7QILiA1EeFmkRf3-KZq0Q2lLtRPam4GMKLkUfBwA/viewform?usp=sf_link");
     }
 
     private void goToUrl(String url) {
