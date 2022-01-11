7650e89fc6d7952df287a678daa26c0658483b2f
Matteo Chiappelloni <matteochiappelloni@gmail.com>
2022-01-11 17:49:28 +0100

    rating lag

--- app/src/main/java/it/sal/disco/unimib/trovacampi/Calcio_activity.java
+++ app/src/main/java/it/sal/disco/unimib/trovacampi/Calcio_activity.java
@@ -20,20 +20,20 @@ public class Calcio_activity extends AppCompatActivity {
         setContentView(R.layout.activity_calcio);
         TextView textView = (TextView) findViewById(R.id.textView3);
         textView.setMovementMethod(LinkMovementMethod.getInstance());
-        textView.setText(Html.fromHtml("<a href=\"https://www.bing.com/maps/directions?rtp=adr.~pos.45.615020751953125_9.26572036743164_Via+Vittorio+Alfieri+32+-+20057+Vedano+Al+Lambro+(MI)_Sportpark_039+249+4846\">VIA VITTORIO ALFIERI 32, VEDANO\n</a>"));
+        textView.setText(Html.fromHtml("<a href=\"https://www.google.it/maps/place/Centro+Sportivo+Sportpark/@45.6183641,9.2783404,14z/data=!4m5!3m4!1s0x4786bbcf7934a365:0x9bb080f0b3a79618!8m2!3d45.6153534!4d9.265605\">VIA VITTORIO ALFIERI 32, VEDANO\n</a>"));
 
         TextView textView1 = (TextView) findViewById(R.id.textView9);
         textView1.setMovementMethod(LinkMovementMethod.getInstance());
-        textView1.setText(Html.fromHtml("<a href=\"https://www.bing.com/local?lid=YN1354x18269578375755280536&id=YN1354x18269578375755280536&q=Cantera+Club+Sport+%26+Restaurant&name=Cantera+Club+Sport+%26+Restaurant&cp=45.63228988647461%7e9.296019554138184&ppois=45.63228988647461_9.296019554138184_Cantera+Club+Sport+%26+Restaurant\">VIA PARCO 51, BIASSONO\n</a>"));
+        textView1.setText(Html.fromHtml("<a href=\"https://www.google.it/maps/place/Cantera+Club+Sport+%26+Restaurant/@45.6305723,9.2835103,17z/data=!3m1!4b1!4m5!3m4!1s0x4786bb4d03095c7d:0x52d8b5ae6fb13b7b!8m2!3d45.6305721!4d9.2856544\">VIA PARCO 51, BIASSONO\n</a>"));
 
-        /*TextView textView = (TextView) findViewById(R.id.TextView01);
-        textView.setMovementMethod(LinkMovementMethod.getInstance());
-        textView.setText(Html.fromHtml("<a href=\"http://www.mrwebmaster.it\">Mr.Webmaster</a>"));
+        TextView textView2 = (TextView) findViewById(R.id.textView10);
+        textView2.setMovementMethod(LinkMovementMethod.getInstance());
+        textView2.setText(Html.fromHtml("<a href=\"https://www.google.it/maps/place/Derbymonza/@45.5885864,9.2537131,17z/data=!4m12!1m6!3m5!1s0x4786b96c89259eb5:0xa675eeef96268b2d!2sDerbymonza!8m2!3d45.5880661!4d9.2539825!3m4!1s0x4786b96c89259eb5:0xa675eeef96268b2d!8m2!3d45.5880661!4d9.2539825\">VIALE LOMBARDIA 17, MONZA</a>"));
 
-        TextView textView = (TextView) findViewById(R.id.TextView01);
-        textView.setMovementMethod(LinkMovementMethod.getInstance());
-        textView.setText(Html.fromHtml("<a href=\"http://www.mrwebmaster.it\">Mr.Webmaster</a>"));
-
+        TextView textView3 = (TextView) findViewById(R.id.textView11);
+        textView3.setMovementMethod(LinkMovementMethod.getInstance());
+        textView3.setText(Html.fromHtml("<a href=\"https://www.google.it/maps/place/Centro+Sportivo+comunale/@45.6624473,9.3026963,17z/data=!3m1!5s0x4786badc82cceb27:0xc487fcf930678914!4m12!1m6!3m5!1s0x4786badd50cc30f9:0x1ba179c508f550e6!2sTijuana+Correzzana!8m2!3d45.6624473!4d9.304885!3m4!1s0x4786badc9b6b9081:0xad250d61ba31393d!8m2!3d45.6623993!4d9.3045858\">VIA PRINCIPALE 38, CORREZZANA</a>"));
+     /*
         TextView textView = (TextView) findViewById(R.id.TextView01);
         textView.setMovementMethod(LinkMovementMethod.getInstance());
         textView.setText(Html.fromHtml("<a href=\"http://www.mrwebmaster.it\">Mr.Webmaster</a>"));
