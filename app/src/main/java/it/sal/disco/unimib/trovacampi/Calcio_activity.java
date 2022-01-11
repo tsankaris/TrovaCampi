2312f5fde85162138708b8d52a4d0bb898e38540
lucagal284 <l.galliani@campus.unimib.it>
2022-01-11 17:19:18 +0100

    niente indiani oggi

--- app/src/main/java/it/sal/disco/unimib/trovacampi/Calcio_activity.java
+++ app/src/main/java/it/sal/disco/unimib/trovacampi/Calcio_activity.java
@@ -1,11 +1,14 @@
 package it.sal.disco.unimib.trovacampi;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 
@@ -14,7 +17,47 @@ public class Calcio_activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calcio); }
+        setContentView(R.layout.activity_calcio);
+        TextView textView = (TextView) findViewById(R.id.textView3);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView.setText(Html.fromHtml("<a href=\"https://www.bing.com/maps/directions?rtp=adr.~pos.45.615020751953125_9.26572036743164_Via+Vittorio+Alfieri+32+-+20057+Vedano+Al+Lambro+(MI)_Sportpark_039+249+4846\">VIA VITTORIO ALFIERI 32, VEDANO\n</a>"));
+
+        TextView textView1 = (TextView) findViewById(R.id.textView9);
+        textView1.setMovementMethod(LinkMovementMethod.getInstance());
+        textView1.setText(Html.fromHtml("<a href=\"https://www.bing.com/local?lid=YN1354x18269578375755280536&id=YN1354x18269578375755280536&q=Cantera+Club+Sport+%26+Restaurant&name=Cantera+Club+Sport+%26+Restaurant&cp=45.63228988647461%7e9.296019554138184&ppois=45.63228988647461_9.296019554138184_Cantera+Club+Sport+%26+Restaurant\">VIA PARCO 51, BIASSONO\n</a>"));
+
+        /*TextView textView = (TextView) findViewById(R.id.TextView01);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView.setText(Html.fromHtml("<a href=\"http://www.mrwebmaster.it\">Mr.Webmaster</a>"));
+
+        TextView textView = (TextView) findViewById(R.id.TextView01);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView.setText(Html.fromHtml("<a href=\"http://www.mrwebmaster.it\">Mr.Webmaster</a>"));
+
+        TextView textView = (TextView) findViewById(R.id.TextView01);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView.setText(Html.fromHtml("<a href=\"http://www.mrwebmaster.it\">Mr.Webmaster</a>"));
+
+        TextView textView = (TextView) findViewById(R.id.TextView01);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView.setText(Html.fromHtml("<a href=\"http://www.mrwebmaster.it\">Mr.Webmaster</a>"));
+
+        TextView textView = (TextView) findViewById(R.id.TextView01);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView.setText(Html.fromHtml("<a href=\"http://www.mrwebmaster.it\">Mr.Webmaster</a>"));
+
+        TextView textView = (TextView) findViewById(R.id.TextView01);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView.setText(Html.fromHtml("<a href=\"http://www.mrwebmaster.it\">Mr.Webmaster</a>"));
+
+        TextView textView = (TextView) findViewById(R.id.TextView01);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView.setText(Html.fromHtml("<a href=\"http://www.mrwebmaster.it\">Mr.Webmaster</a>"));
+
+        TextView textView = (TextView) findViewById(R.id.TextView01);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        textView.setText(Html.fromHtml("<a href=\"http://www.mrwebmaster.it\">Mr.Webmaster</a>"));*/
+    }
 
 
 
@@ -27,4 +70,6 @@ public class Calcio_activity extends AppCompatActivity {
         Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
         startActivity(launchBrowser);
     }
+
+
 }
--- app/src/main/java/it/sal/disco/unimib/trovacampi/MapsFragment.java
+++ app/src/main/java/it/sal/disco/unimib/trovacampi/MapsFragment.java
@@ -1,13 +1,13 @@
 package it.sal.disco.unimib.trovacampi;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
